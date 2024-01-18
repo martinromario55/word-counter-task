aa980e641938aac4a6fb86e36960e8f2f839f2c0
martinromario55 <martinromario55@gmail.com>
2024-01-18 21:55:57 +0300

    Page Not Found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <div className='pnfimage'>
+      <img
+        src="https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-template-3.png"
+        alt="page not found"
+      />
+    </div>
   )
 }
